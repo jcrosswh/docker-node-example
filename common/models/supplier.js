65ae29735b7e689236e6beceb10a674ed9bfcf02
Joel Crosswhite <jcrosswhite@arrow.com>
2016-11-29 10:33:32 -0700

    Added logic for inserting suppliers via sproc call.

--- common/models/supplier.js
+++ common/models/supplier.js
@@ -1,5 +1,21 @@
 'use strict';
 
-module.exports = function(Supplier) {
+module.exports = function (Supplier) {
 
+  Supplier.on('dataSourceAttached', function (obj) {
+
+    var connection = Supplier.dataSource.app.dataSources.ecsdev.connector;
+
+    Supplier.create = function (data, cb) {
+
+      var sql = "BEGIN jc_test.create_supplier(:name); END;";
+
+      connection.execute(sql, [data.name], function (err, result) {
+        if (err) {
+          console.log("Error:", err);
+        }
+        cb(err, data);
+      });
+    };
+  });
 };
